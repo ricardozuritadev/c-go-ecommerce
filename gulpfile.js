54fa222594050c88b75fbfc9e63155c353809b78
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-08 09:55:03 +0100

    Implement browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,53 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
-gulp.task('sass', function () {
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch('./scss/*.scss').on('change', reload);
+  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./js/**/*.js').on('change', reload);
+  gulp.watch('./js/**/*.js', gulp.series('js'));
+});
+
+gulp.task('css', () => {
+  return gulp
+    .src('./scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', () => {
   return gulp
     .src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('js', function () {
+gulp.task('js', () => {
   return gulp
     .src('./js/**/*.js')
     .pipe(concat('bundle.js'))
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', gulp.series('sass'));
-  gulp.watch('./js/**/*.js', gulp.series('js'));
-});
+gulp.task('watch', gulp.series('browser-sync'));
 
-gulp.task('default', gulp.series('sass', 'js', 'watch'));
+gulp.task('default', gulp.series('css', 'sass', 'js', 'watch'));
