36a7ac81f816a283961fd05c66241c5ae9c79f50
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-03-08 11:09:52 +0100

    Create selected styles functionality

--- js/render-data.js
+++ js/render-data.js
@@ -1,4 +1,5 @@
 const API_URL = 'https://api.npoint.io/5f458ccb947908d10993';
+const navList = document.querySelector('.nav__list');
 
 window.addEventListener('DOMContentLoaded', () => {
   fetchData(API_URL);
@@ -15,10 +16,9 @@ async function fetchData(url) {
 }
 
 function renderNavItems(data) {
-  const navList = document.querySelector('.nav__list');
   let navItems = ``;
 
-  navList.innerHTML = data?.map((item) => {
+  navList.innerHTML = data?.map((item, index) => {
     navItems += `
         <li class="nav__items">
           <a href="#${item.Name}">${item.Name}</a>
@@ -27,4 +27,22 @@ function renderNavItems(data) {
   });
 
   navList.innerHTML = navItems;
+
+  // Add onClick event to each nav item
+  const liItems = navList.querySelectorAll('.nav__items');
+  liItems.forEach((item) => {
+    item.addEventListener('click', function () {
+      liItems.forEach((item) => {
+        item.classList.remove('nav__items--selected');
+      });
+
+      // Add "selected" class to the clicked nav item
+      this.classList.add('nav__items--selected');
+    });
+  });
+
+  // Select the first nav item by default
+  if (liItems.length > 0) {
+    liItems[0].classList.add('nav__items--selected');
+  }
 }
